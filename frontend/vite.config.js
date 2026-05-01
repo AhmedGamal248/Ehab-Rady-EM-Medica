3b58e56e716253fd5cfe1029350e2955800578fd
gamal <ag2849315@gmail.com>
2026-05-01 19:25:34 +0300

    edit vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['react-icons', 'react-hot-toast'],
-          'i18n': ['i18next', 'react-i18next'],
+        manualChunks(id) {
+          if (!id.includes('node_modules')) return
+
+          if (
+            id.includes('react') ||
+            id.includes('react-dom') ||
+            id.includes('react-router-dom')
+          ) {
+            return 'react-vendor'
+          }
+
+          if (
+            id.includes('react-icons') ||
+            id.includes('react-hot-toast')
+          ) {
+            return 'ui-vendor'
+          }
+
+          if (
+            id.includes('i18next') ||
+            id.includes('react-i18next')
+          ) {
+            return 'i18n'
+          }
+
+          return 'vendor'
         },
       },
     },
